2dbfd8ecfad0a36ba0837ef3b9381cadbbeb90ad
Bupalchow <bupalchowdary06@gmail.com>
2024-01-21 14:59:15 +0530

    Done

--- constants/constants.ts
+++ constants/constants.ts
@@ -1,8 +1,9 @@
 export const NFT_CONTRACT_ADDRESS = "0x77759Ccc569db4B29975bBD160Bc89EF48B15CaE";
 
 export const NFT_CHECKOUT_LINKS = [
-    "https://payments.thirdweb.com/checkout/7d904070-025e-4754-8d33-88f042bf3d26" ,
-    "https://payments.thirdweb.com/checkout/7d904070-025e-4754-8d33-88f042bf3d26" ,
-    "https://payments.thirdweb.com/checkout/7d904070-025e-4754-8d33-88f042bf3d26" ,
-    "https://payments.thirdweb.com/checkout/7d904070-025e-4754-8d33-88f042bf3d26" ,
+    "https://payments.thirdweb.com/checkout/ec69611b-050a-4be6-9e03-2c0303e88b2e",
+    "https://payments.thirdweb.com/checkout/b6278677-a163-462b-9760-a28ddf8d0087",
+    "https://payments.thirdweb.com/checkout/1ad724ec-152f-43f2-ab9f-b8848cf1eadd",
+    "https://payments.thirdweb.com/checkout/ea87d36d-b285-425d-84ef-36ce79c3dd8c"
+
 ];
